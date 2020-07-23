447fc07af6701d0099ebc0dfc38ad0383dd22dc4
Miguel Angel <matr05viru@gmail.com>
2020-07-23 21:31:29 +0200

    feat: added prisma

---
+++ prisma/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://fc-prisma-server-bfb157d7d9.herokuapp.com/fc-prisma-service/dev`
+});
+exports.prisma = new exports.Prisma();
