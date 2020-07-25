2a3ea475b8b777cf8bd4e6021e8ba682bc792636
Miguel Angel <matr05viru@gmail.com>
2020-07-26 00:37:30 +0200

    feat: added more api operations

--- index.js
+++ index.js
@@ -3,38 +3,84 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-let movies = [
-  {
-    id: 1,
-    title: "Joker",
-  },
-  {
-    id: 2,
-    title: "Avengers: Infinity war",
-  },
-  {
-    id: 3,
-    title: "Gladiator",
-  },
-  {
-    id: 4,
-    title: "Robin Hood",
-  },
-];
-
 const resolvers = {
   Query: {
-    getAllmovies: async (parent, args, context) => {
-      console.log("args", context);
+    getAllMovies: async (parent, args, context) => {
       return await context.prisma.movie.findMany();
+    },
+    getAllShareholders: async (parent, args, context) => {
+      return await context.prisma.shareholder.findMany({
+        include: {
+          Movie: true,
+          BalanceTransaction: true,
+        },
+      });
+    },
+    getAllTransferByMovie: async (parent, args, context) => {
+      return await context.prisma.transfer.findMany({
+        where: { Movie: { id: args.movieId } },
+      });
     },
   },
   Mutation: {
     createMovie: async (parent, args, context) => {
-      console.log("create movie", args);
       return await context.prisma.movie.create({
         data: { title: args.title },
       });
+    },
+    createShareholder: async (parent, args, context) => {
+      const shareholder = await context.prisma.shareholder.create({
+        data: {
+          firstName: args.firstName,
+          lastName: args.lastName,
+          address: args.address,
+          iban: args.iban,
+          Movie: {
+            connect: { id: args.movieId },
+          },
+        },
+        include: { Movie: true, BalanceTransaction: true },
+      });
+      return shareholder;
+    },
+    createTransfer: async (parent, args, context) => {
+      /* Step 1: Create transfer */
+      const transfer = await context.prisma.transfer.create({
+        data: {
+          amount: args.amount,
+          description: args.description,
+          Movie: {
+            connect: { id: args.movieId },
+          },
+        },
+        include: { Movie: true },
+      });
+
+      /* Step 2: Find how many shareholders belongs to the film related to the transfer previously created. */
+      const shareholdersByFilm = await context.prisma.shareholder.findMany({
+        select: { id: true },
+        where: { Movie: { id: args.movieId } },
+      });
+
+      /* Step 3: Divide the amount among all the shareholders involved. */
+      const shareholdersCount = shareholdersByFilm.length;
+      const balanceQuantity = args.amount / shareholdersCount;
+
+      /* Step 4: Create a balance transaction for each shareholder related to the transfer created. */
+      shareholdersByFilm.map(async (shareholder) => {
+        await context.prisma.balanceTransaction.create({
+          data: {
+            amount: balanceQuantity,
+            Transfer: {
+              connect: { id: transfer.id },
+            },
+            Shareholder: {
+              connect: { id: shareholder.id },
+            },
+          },
+        });
+      });
+      return transfer;
     },
   },
 };
